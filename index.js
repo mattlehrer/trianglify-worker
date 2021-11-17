39bc402346f422dba0931672a9cd37a3e8caaed8
Matt Lehrer <lehrerm@gmail.com>
2021-11-17 20:49:43 +0100

    working for svg

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
+const trianglify = require('trianglify');
+
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
+  event.respondWith(handleRequest(event.request));
+});
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  console.log({ request });
+
+  const url = request.url.split('/');
+
+  let seed;
+
+  let triangle;
+  try {
+    seed = url[url.length - 1].split('.')[0];
+  } catch (error) {
+    seed = '0x0000000000000000000000000000000000000000';
+  }
+
+  triangle = trianglify({
+    width: 256,
+    height: 256,
+    seed,
+  });
+  const headers = { 'Content-Type': 'image/svg+xml' };
+  const svg = triangle.toSVG();
+
+  return new Response(svg, { headers });
 }
